51438fe96f058412ebf96b852900843bda2d6f7e
Sasitha Layan Yasathilaka <onlinesasitha@gmail.com>
2020-05-24 12:16:30 +0530

    push to heroku

--- Routes/PromoRoutes.js
+++ Routes/PromoRoutes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const {create, read, search, singlePromoCode, promoById, remove, update} = require("../Controllers/PromoController"); 
-const { requireSignin, isAuth, isAdmin } = require("../Controllers/auth");
+const { requireSignin, isAuth, isAdmin, isStoreManager } = require("../Controllers/auth");
 const { userById } = require("../Controllers/UserController");
 
-router.post("/admin/addPromocode/:userId", requireSignin, isAuth, isAdmin, create);
-router.get("/admin/getPromocodes/:userId", requireSignin, isAuth, isAdmin, read);
-router.get("/admin/promocodes/:userId/search", requireSignin, isAuth, isAdmin, search);
+router.post("/admin/addPromocode/:userId", requireSignin, isAuth, isStoreManager, create);
+router.get("/admin/getPromocodes/:userId", requireSignin, isAuth, isStoreManager, read);
+router.get("/admin/promocodes/:userId/search", requireSignin, isAuth, isStoreManager, search);
 router.get("/admin/getSinglePromocode/:userId/search", requireSignin, isAuth, singlePromoCode);
-router.delete("/admin/deletePromocode/:userId/:promoId", requireSignin, isAuth, isAdmin, remove);
-router.put("/admin/updatePromocode/:userId/:promoId", requireSignin, isAuth, isAdmin, update);
+router.delete("/admin/deletePromocode/:userId/:promoId", requireSignin, isAuth, isStoreManager, remove);
+router.put("/admin/updatePromocode/:userId/:promoId", requireSignin, isAuth, isStoreManager, update);
 
 router.param("userId", userById);
 router.param("promoId", promoById);
